458b06ccfa0ac81e235ad643e61d4fffcc5236da
Christopher Schwardt <nookieman@gmx.de>
2011-12-30 21:18:29 +0100

    bugfix using the language database model in series

--- mediaExchange/series/models.py
+++ mediaExchange/series/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import get_model
 
 from mediaExchange.movies.models import Item, Language
 
@@ -14,12 +15,6 @@ class SerieSource(models.Model):
     def __unicode__(self): 
         return self.name 
 
-class Language(models.Model): 
-    name = models.CharField(max_length=256, blank=True, null=True) 
- 
-    def __unicode__(self): 
-        return self.name 
-
 class Serie(models.Model):
     name = models.CharField(max_length=256)
 
@@ -30,7 +25,7 @@ class Season(Item):
     serie = models.ForeignKey('Serie')
     subname = models.CharField(max_length=256, blank=True, null=True)
     number = models.IntegerField(blank=False, null=False)
-    language = models.ForeignKey('Language', blank=True, null=True)
+    language = models.ForeignKey(get_model('movies', 'Language'), blank=True, null=True)
     genre = models.ForeignKey('SerieGenre', blank=True, null=True)
     source = models.ForeignKey('SerieSource', blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
