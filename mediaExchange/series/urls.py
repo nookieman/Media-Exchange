fca1bbab4ad280db595e657a224837c1143e3cfc
Christopher Schwardt <nookieman@gmx.de>
2011-12-30 21:17:50 +0100

    bugfix in series urls

--- mediaExchange/series/urls.py
+++ mediaExchange/series/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mediaExchange.series.views',
     (r'^$', 'seriesindex'),
-    (r'^series/(?P<serie_id>\d)/?$', 'seriesseriedetails'),
-    (r'^season/(?P<season_id>\d)/?$', 'seriesseasondetails'),
-    (r'^create/(?P<season_id>\d)/?$', 'seriesseasoncreate'),
+    (r'^series/(?P<serie_id>\d+)/?$', 'seriesseriedetails'),
+    (r'^season/(?P<season_id>\d+)/?$', 'seriesseasondetails'),
+    (r'^create/(?P<season_id>\d+)/?$', 'seriesseasoncreate'),
 )
