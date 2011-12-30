4f0efe82e6624325bb701fdd1c85df8e0432067f
Christopher Schwardt <nookieman@gmx.de>
2011-12-30 17:10:36 +0100

    made series appear on index: 20 newest series

--- mediaExchange/index/views.py
+++ mediaExchange/index/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from mediaExchange.movies.models import Movie, UploadRequest, DownloadFile
+from mediaExchange.series.models import Season
 
 @login_required
 def indexindex(request):
+#    items = Items.objects.all().order_by('-id')[:20]
     movies = Movie.objects.all().order_by('-id')[:20]
+    seasons = Season.objects.all().order_by('-id')[:20]
     urs = UploadRequest.objects.filter(done=False).order_by('id')
     doneurs = UploadRequest.objects.filter(done=True)
-    return render_to_response('index/index.html', {'movies':movies, 'uploadRequests':urs, 'doneUploadRequests':doneurs})
+    return render_to_response('index/index.html', {'movies':movies, 'seasons':seasons, 'uploadRequests':urs, 'doneUploadRequests':doneurs})
