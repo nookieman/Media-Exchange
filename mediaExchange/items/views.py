4b033d31f5ed2aa23ae17e31ecefa7db78dcd68f
Christopher Schwardt <nookieman@gmx.de>
2011-12-31 14:11:18 +0100

    also show series seasons in /items/ view

--- mediaExchange/items/views.py
+++ mediaExchange/items/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 
 from mediaExchange.movies.models import Item, Movie, UploadRequest, DownloadFile
 from mediaExchange.movies.views import moviesdetails
+from mediaExchange.series.models import Season
+from mediaExchange.series.views import seriesseasondetails
 from mediaExchange.index.views import indexindex
 
 @login_required
@@ -11,6 +13,8 @@ def itemsdetails(request, itemid):
     item = get_object_or_404(Item, pk=itemid)
     if Movie.objects.filter(id=itemid):
         response = moviesdetails(request, itemid)
+    elif Season.objects.filter(id=itemid):
+        response = seriesseasondetails(request, itemid)
     else:
         response = indexindex(request)
     return response
