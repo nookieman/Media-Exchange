3d59b869a886afc1bc3e744048c0c27fe848716e
Christopher Schwardt <nookieman@gmx.de>
2012-01-20 22:36:55 +0100

    adopted new url parsing patterns

--- mediaExchange/movies/urls.py
+++ mediaExchange/movies/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('mediaExchange.movies.views',
     (r'^$', 'moviesindex'),
     (r'^details/(?P<movie_id>\d+)/?$', 'moviesdetails'),
-    (r'^create/(?P<movie_id>\d+)/?$', 'moviescreate'),
-    (r'^request/(?P<movie_id>\d+)/?$', 'moviesrequest'),
-    (r'^addlinks/(?P<movie_id>\d+)/?$', 'moviesaddlinks'),
+    (r'^create/(?P<item_instance_id>\d+)/?$', 'moviescreate'),
+    (r'^request/(?P<item_instance_id>\d+)/?$', 'moviesrequest'),
+    (r'^addlinks/(?P<item_instance_id>\d+)/?$', 'moviesaddlinks'),
 )
 
