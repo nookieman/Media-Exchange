fbd1dc9a17d8d43a559181a556ca340e68100feb
Christopher Schwardt <nookieman@gmx.de>
2012-01-20 19:25:17 +0100

    export the new model structure

--- mediaExchange/exporter/views.py
+++ mediaExchange/exporter/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-from mediaExchange.movies.models import Movie, UploadRequest, DownloadFileGroup, DownloadFile, EncryptionKey
+from mediaExchange.movies.models import Movie, UploadRequest, DownloadFileGroup, DownloadFile, EncryptionKey, ItemInstance
 from mediaExchange.series.models import Season
 
 @login_required
@@ -15,25 +15,36 @@ def exporterexport(request):
     {
         'keys' : {'1' : {'chunkSize' : ..., 'key' : ...}, ... },
         'movies' : [ {
-                        'id' : ...,
-                        'name' : ...,
-                        'size' : ...,
-                        'subname' : ...
-                        'language' : ...,
-                        'genre'  : ...,
-                        'source' : ...,
-                        ''}, ...],
+                      'id' : ...,
+                      'name' : ...,
+                      'subname' : ...
+                      'genre'  : ...,
+                      'instance' : [ {
+                                      'size' : ...,
+                                      'language' : ...,
+                                      'source' : ...,
+                                      'downloadFileGroups' : [{
+                                                               'downloadLinks': [...],
+                                                               'key': ...
+                                                             }, ...],
+                                   }, ... ],
+                   }, ...],
         'series' : [ {
-                        'id' : ...,
-                        'name' : ...,
-                        'size' : ...,
-                        'number' : ...,
-                        'subname' : ...,
-                        'language' : ...,
-                        'genre'  : ...,
-                        'source' : ...,
-                        'downloadFileGroups' : [{'downloadLinks': [...], 'key': ...}, ...],
-                     }, ...]
+                      'id' : ...,
+                      'name' : ...,
+                      'number' : ...,
+                      'subname' : ...,
+                      'genre'  : ...,
+                      'instance' : [ {
+                                      'size' : ...,
+                                      'language' : ...,
+                                      'source' : ...,
+                                      'downloadFileGroups' : [{
+                                                               'downloadLinks': [...],
+                                                               'key': ...
+                                                             }, ...],
+                                   }, ... ],
+                   }, ...]
     }
     """
     jsonStruct = createExportStruct(movies=Movie.objects.all(),
@@ -53,16 +64,19 @@ def _jsonStructFromItemList(items, onlyDownloadable=False):
     itemStruct = []
     for item in items:
         itemDict = item.toDict()
-        downloadFileGroups = DownloadFileGroup.objects.filter(item=item)
-        if downloadFileGroups:
-            itemDict['downloadFileGroups'] = []
-            for downloadFileGroup in downloadFileGroups:
+        instancesList = []
+        for instance in ItemInstance.objects.filter(item=item):
+            instanceDict = instance.toDict()
+            downloadFileGroupsList = []
+            for downloadFileGroup in DownloadFileGroup.objects.filter(itemInstance=instance):
                 downloadFiles = DownloadFile.objects.filter(downloadFileGroup=downloadFileGroup)
                 downloadLinks = []
                 for downloadFile in downloadFiles:
                     downloadLinks.append(downloadFile.downloadLink)
-                itemDict['downloadFileGroups'].append({'downloadLinks' : downloadLinks,
-                                                       'key'           : downloadFileGroup.key.id})
+                downloadFileGroupsList.append({'downloadLinks' : downloadLinks,
+                                               'key'           : downloadFileGroup.key.id})
+            instanceDict['downloadFileGroups'] = downloadFileGroupsList
+        itemDict['instances'] = instancesList
         itemStruct.append(itemDict)
     return itemStruct
 
