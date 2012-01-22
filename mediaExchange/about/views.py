6ce864940b2f701e3e6d7a24e38fcdc0504be642
Christopher Schwardt <nookieman@gmx.de>
2012-01-22 17:31:19 +0100

    fix: corrected import in about

--- mediaExchange/about/views.py
+++ mediaExchange/about/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-from mediaExchange.movies.models import UploadRequest
+from mediaExchange.items.models import UploadRequest
 from mediaExchange.settings import ENCRYPTION_KEY, ENCRYPTION_CHUNK_SIZE
 
 @login_required
