55069b075b962e47a460f14e5af4dd72dbd5619e
Christopher Schwardt <nookieman@gmx.de>
2012-01-22 17:10:04 +0100

    only use generic item view
    deleted files and functions not needed anymore
    replaced all links to new generic items view

--- mediaExchange/series/urls.py
+++ mediaExchange/series/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('mediaExchange.series.views',
     (r'^$', 'seriesindex'),
     (r'^series/(?P<serie_id>\d+)/?$', 'seriesseriedetails'),
-    (r'^season/(?P<season_id>\d+)/?$', 'seriesseasondetails'),
 )
--- mediaExchange/series/views.py
+++ mediaExchange/series/views.py
@@ -19,28 +19,9 @@ def seriesseriedetails(request, serie_id):
     serie = get_object_or_404(Serie, pk=serie_id)
     return getSerieDetails(serie)
 
-@login_required
-def seriesseasondetails(request, season_id):
-    season = get_object_or_404(Season, pk=season_id)
-    return getSeasonDetails(request, season)
-
 def getSerieDetails(serie):
     seasons = Season.objects.filter(serie=serie)
     urs = UploadRequest.objects.filter(done=False).order_by('id')
     doneurs = UploadRequest.objects.filter(done=True)
     return render_to_response('series/seriesdetails.html', {'serie':serie, 'seasons':seasons, 'uploadRequests':urs, 'doneUploadRequests':doneurs})
-
-def getSeasonDetails(request, season, message=None):
-    c = {}
-    itemInstances = ItemInstance.objects.filter(item=season)
-    urs = UploadRequest.objects.filter(done=False).order_by('id')
-    form = AddItemUploadForm()
-    c.update({'serie'              : season.serie,
-              'season'             : season,
-              'itemInstances'      : itemInstances,
-              'uploadRequests'     : urs,
-              'message'            : message,
-              'addItemForm'        : form})
-    c.update(csrf(request))
-    return render_to_response('series/seasondetails.html', c)
 
