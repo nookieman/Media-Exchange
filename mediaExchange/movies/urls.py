6ebc077e8178fb18f55e21dc6e177dd46363dc46
Christopher Schwardt <nookieman@gmx.de>
2012-01-25 09:52:26 +0100

    removed unused url

--- mediaExchange/movies/urls.py
+++ mediaExchange/movies/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mediaExchange.movies.views',
     (r'^$', 'moviesindex'),
-    (r'^details/(?P<movie_id>\d+)/?$', 'moviesdetails'),
 )
 
