fc3cc88dbae04745c1d7c2099714f5921a1f67e8
Christopher Schwardt <nookieman@gmx.de>
2012-12-08 20:40:44 +0100

    add audios to index

--- mediaExchange/index/views.py
+++ mediaExchange/index/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from mediaExchange.items.models import Movie, UploadRequest, Season
+from mediaExchange.items.models import Audio, Movie, UploadRequest, Season
 
 @login_required
 def indexindex(request):
 #    items = Items.objects.all().order_by('-id')[:20]
     movies = Movie.objects.all().order_by('-id')[:20]
     seasons = Season.objects.all().order_by('-id')[:20]
+    audios = Audio.objects.all().order_by('-id')[:20]
     urs = UploadRequest.objects.filter(done=False).order_by('id')
     doneurs = UploadRequest.objects.filter(done=True)
-    return render_to_response('index/index.html', {'movies':movies, 'seasons':seasons, 'uploadRequests':urs, 'doneUploadRequests':doneurs})
+    return render_to_response('index/index.html', {'audios': audios,
+                                                   'movies':movies,
+                                                   'seasons':seasons,
+                                                   'uploadRequests':urs,
+                                                   'doneUploadRequests':doneurs})
